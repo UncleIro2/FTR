226a15b56c2704e872174cf341ecb2147a629c71
ItsMeMarcoo <marc297a@edu.nextkbh.dk>
2024-03-19 09:59:07 +0100

    Forside der ændreso m lidt

---
+++ Assets/Scenes/Scripts/JumpScenes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour
+
+
+{
+    public void LoadSampleScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void LoadGuidlines()
+    {
+        SceneManager.LoadScene("Guidlines");
+    }
+}
+
