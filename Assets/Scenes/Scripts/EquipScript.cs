db3b5b014563e3d23d862a1c3b7e2790aca723ab
UncleIro2 <146819046+UncleIro2@users.noreply.github.com>
2024-05-03 10:43:29 +0200

    Equip script done

--- Assets/Scenes/Scripts/EquipScript.cs
+++ Assets/Scenes/Scripts/EquipScript.cs
@@ -6,139 +6,72 @@ using UnityEngine;
 public class EquipScript : MonoBehaviour
 {
     public Transform PlayerTransform;
-    public GameObject Brændslukker;
-    public GameObject Brandtæpperør;
+    public GameObject obj;
     public Camera Camera;
-    public Rigidbody rbBrændslukker;
-    public Rigidbody rbBrandtæpperør;
+    public Rigidbody rb;
     public bool isholding = false ;
  
-
-    void Start()
+    void Update()
     {
-        rbBrændslukker = Brændslukker.GetComponent<Rigidbody>();
-        rbBrandtæpperør = Brandtæpperør.GetComponent<Rigidbody>();
+        if (obj != null)
+        {
+            rb = obj.GetComponent<Rigidbody>();
+        }
+        KeyBind();
+  
     }
 
-    // Update is called once per frame
-    void Update()
+    void Equip()
+    { 
+        if (obj != null && (obj.transform.position - PlayerTransform.transform.position).magnitude <= new Vector3(1f, 1f, 1f).magnitude && isholding == false) 
+        {
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            obj.transform.position = PlayerTransform.transform.position;
+            obj.transform.rotation = PlayerTransform.transform.rotation;
+            obj.transform.SetParent(PlayerTransform);
+            isholding = true;
+
+        }
+    }
+
+    void Unequip()
+    { 
+        if(isholding == true) 
+        {
+
+            rb.constraints = RigidbodyConstraints.None;
+            PlayerTransform.DetachChildren();
+            obj.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            isholding = false;
+      ;
+        }
+    }
+
+
+    void KeyBind()
     {
         if (Input.GetKeyDown("e"))
         {
-            if (isholding == false)
-            {
-                EquipBrændslukker();
-                isholding = true;
-            }
-            if (isholding == false)
-            {
-                EquipBrandtæpperør();
-                isholding = true;
-
-
-            }
-
-          
+           
+            Equip();            
+            
 
 
         }
-        
-     
+         if (Input.GetKeyDown("q"))
+         {
 
-
-
-
-        if (Input.GetKeyDown("q"))
-        {
-            if(isholding == true)
-            {
-                UnequipBrændslukker();
-                isholding = false;
-            }
-            if(isholding == true)
-            {
-                UnequipBrandtæpperør();
-                isholding = false;
-            }
-
-
-
-        }
-
-
-
+            Unequip();
+   
+         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("obj")) {
-            Brændslukker = other.gameObject;
-        }
-    }
-
-    void EquipObeject()
-    {
-       
-
-        
-
-    }
-
-    void UnequipObject()
-    {
-
-
-    }
-
-
-    void UnequipBrændslukker()
-    {
-        rbBrændslukker.constraints = RigidbodyConstraints.None;
-        PlayerTransform.DetachChildren();
-        Brændslukker.transform.eulerAngles = new Vector3(0f, 180f, 0f);
-
-
-        
-
-
-    }
-
-    void UnequipBrandtæpperør()
-    {
-        rbBrandtæpperør.constraints = RigidbodyConstraints.None;
-        PlayerTransform.DetachChildren();
-        Brandtæpperør.transform.eulerAngles = new Vector3(0f, 180f, 0f);
-
-    }
-
-
-
-    void EquipBrændslukker()
-    {
-        if ((Brændslukker.transform.position - PlayerTransform.transform.position).magnitude <= new Vector3(1f, 1f, 1f).magnitude)
+        if (other.gameObject.CompareTag("obj") && !isholding)
         {
-            rbBrændslukker.constraints = RigidbodyConstraints.FreezeAll;
-            Brændslukker.transform.position = PlayerTransform.transform.position;
-            Brændslukker.transform.rotation = PlayerTransform.transform.rotation;
-            Brændslukker.transform.SetParent(PlayerTransform);
-
+            obj = other.gameObject;
         }
-
-
-
-    }
-
-    void EquipBrandtæpperør()
-    {
-        if ((Brandtæpperør.transform.position - PlayerTransform.transform.position).magnitude <= new Vector3(1f, 1f, 1f).magnitude)
-        {
-            rbBrandtæpperør.constraints = RigidbodyConstraints.FreezeAll;
-            Brandtæpperør.transform.position = PlayerTransform.transform.position;
-            Brandtæpperør.transform.rotation = PlayerTransform.transform.rotation;
-            Brandtæpperør.transform.SetParent(PlayerTransform);
-        }
-
-
     }
 
 }
