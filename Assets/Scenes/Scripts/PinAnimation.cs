c1c47cf568069226df8ec50950b24403c2d0fcd0
ItsMeMarcoo <marc297a@edu.nextkbh.dk>
2024-05-06 13:35:16 +0200

    pIN DER VIrker

---
+++ Assets/Scenes/Scripts/PinAnimation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+
+public class AnimatorScript : MonoBehaviour
+
+{
+    public Animator pin; // Reference to the Animator component of the object you want to animate
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(RunAnimation());
+        }
+
+       
+    }
+
+    IEnumerator RunAnimation()
+    {
+        pin.SetBool("PlayAnim", true);
+        float waitTime = GetComponent<Animator>().runtimeAnimatorController.animationClips[0].length;
+        yield return new WaitForSeconds(waitTime);
+        pin.SetBool("AnimationDone", true);
+        Destroy(this.gameObject);
+    }
+}
+    
+   
+
+
