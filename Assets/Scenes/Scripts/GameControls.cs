d8f059ee7b9e3f7223cbc2627a864501c8d91470
ItsMeMarcoo <marc297a@edu.nextkbh.dk>
2024-05-14 10:41:43 +0200

    nYE VLG

--- Assets/Scenes/Scripts/GameControls.cs
+++ Assets/Scenes/Scripts/GameControls.cs
@@ -39,6 +39,42 @@ public class KeyAction : MonoBehaviour
                 // Perform the action when '4' key is pressed
                 SoundMananger.instance.PlaySound(SoundEnum.bip);
             }
+            // Check if '5' key is pressed
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                // Perform the action when '5' key is pressed
+                SoundMananger.instance.PlaySound(SoundEnum.bip);
+            } 
+            // Check if '6' key is pressed
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                // Perform the action when '6' key is pressed
+                SoundMananger.instance.PlaySound(SoundEnum.bip);
+            }
+            // Check if '7' key is pressed
+            if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                // Perform the action when '7' key is pressed
+                SoundMananger.instance.PlaySound(SoundEnum.bip);
+            }
+            // Check if '8' key is pressed
+            if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                // Perform the action when '8' key is pressed
+                SoundMananger.instance.PlaySound(SoundEnum.bip);
+            }
+            // Check if '9' key is pressed
+            if (Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                // Perform the action when '9' key is pressed
+                SoundMananger.instance.PlaySound(SoundEnum.bip);
+            }
+            // Check if '0' key is pressed
+            if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                // Perform the action when '0' key is pressed
+                SoundMananger.instance.PlaySound(SoundEnum.bip);
+            }
         }
        
       
